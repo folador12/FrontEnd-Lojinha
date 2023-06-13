ea3846961295f608f65b77d11e449e4cd85b2b7e
Eder Queiroz <62737156+Eder-Queiroz@users.noreply.github.com>
2023-06-13 11:18:23 -0300

    start project with vite

--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-import ReactDOM from 'react-dom/client';
-import "bootstrap/dist/css/bootstrap.min.css";
-import router from "./routes";
-import { RouterProvider } from "react-router-dom";
-
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
-root.render(<RouterProvider router={router} />);
